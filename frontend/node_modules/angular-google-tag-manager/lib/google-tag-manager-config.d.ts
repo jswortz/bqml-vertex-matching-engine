eb051888e9c829e0a70872c5b435a6208bb1687c
Navie Vurdien <nvurdien@google.com>
2021-01-08 16:05:45 -0800

    remove files

--- frontend/node_modules/angular-google-tag-manager/lib/google-tag-manager-config.d.ts
+++
@@ -1,6 +0,0 @@
-export interface GoogleTagManagerConfig {
-    id: string | null;
-    gtm_auth?: string;
-    gtm_preview?: string;
-    [key: string]: string | null | undefined;
-}
--- frontend/node_modules/angular-google-tag-manager/public-api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/angular-google-tag-manager.service';
-export * from './lib/angular-google-tag-manager.module';
-export * from './lib/google-tag-manager-config';
