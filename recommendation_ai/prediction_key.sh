253bbbc4168a2e27891ef3d798383df8afb28d02
Navie Vurdien <nvurdien@google.com>
2021-02-23 15:17:06 -0800

    udpated prediction key script

--- recommendation_ai/prediction_key.sh
+++ recommendation_ai/prediction_key.sh
@@ -1,9 +1,8 @@
 curl -X POST \
- -H "Authorization: Bearer $(gcloud auth print-access-token YOUR-SERVICE-ACCOUNT)" \ #update with your service account
+ -H "Authorization: Bearer $(gcloud auth print-access-token YOUR-SERVICE-ACCOUNT)" \
  -H "Content-Type: application/json; charset=utf-8" \
  --data "{
        "predictionApiKeyRegistration": {
-            "apiKey": 'API-KEY' # update with your api-key
+            "apiKey": 'API-KEY'
         }
-    }"
- "https://recommendationengine.googleapis.com/v1beta1/projects/css-storeops/locations/global/catalogs/default_catalog/eventStores/default_event_store/predictionApiKeyRegistrations"
+    }" "https://recommendationengine.googleapis.com/v1beta1/projects/PROJECT-ID/locations/global/catalogs/default_catalog/eventStores/default_event_store/predictionApiKeyRegistrations"
