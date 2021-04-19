cd6ea3bb5a12c7de6484df48b3e6afecfbd5e206
Guilherme Fuzatto <fuzatto@google.com>
2021-04-19 20:12:54 +0000

    search response changed to use type

--- app/src/retail/search_benchmark.py
+++ app/src/retail/search_benchmark.py
@@ -2,6 +2,8 @@ from src.config import ELASTICSEARCH_CLOUD_ID
 from src.config import ELASTICSEARCH_INDEX
 from src.config import ELASTICSEARCH_USER
 from src.config import ELASTICSEARCH_PASSWORD
+from src.types.retail import SearchResponse
+from src.types.retail import SearchProduct
 
 from datetime import datetime, timedelta
 from elasticsearch import Elasticsearch
@@ -14,25 +16,6 @@ _client = Elasticsearch(
 
 _MAX_PAGE_SIZE = 120
 _SEARCH_FIELD = 'NAME'
-_FIELD_NAMES = [
-    'AVAILABILITY',
-    'BRAND',
-    'CATEGORY',
-    'COLOR',
-    'COMPANY',
-    'ESTIMATED_DELIVERY_DAY',
-    'ID',
-    'image_link',
-    'NAME',
-    'RETAIL_PRICE',
-    'REVIEW',
-    'SIZE',
-    'SUB_CATEGORY'
-]
-_FIELD_RENAME_MAP = {
-    'image_link': 'IMAGE LINK',
-    'RETAIL_PRICE': 'PRICE'
-}
 
 def _autocorrect(user_query):
     
@@ -52,22 +35,6 @@ def _autocorrect(user_query):
     
     return ' '.join(search_args)
 
-def _format_hit(row_dict):
-
-    row = {k:v for k, v in row_dict.items() if k in _FIELD_NAMES}
-
-    for old_key, new_key in _FIELD_RENAME_MAP.items():
-        row[new_key] = row.pop(old_key)
-
-    days = row.pop('ESTIMATED_DELIVERY_DAY')
-
-    dt = datetime.utcnow() + timedelta(days=days)
-    dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
-
-    row['ESTIMATE_DELIVERY_AT'] = math.floor(dt.timestamp() * 1000)
-
-    return row
-
 
 def search_benchmark(
     query='',
@@ -86,12 +53,14 @@ def search_benchmark(
         [(page - 1) * page_size : page * page_size]
         .execute())
 
-    response = {
-        'numPages': math.ceil(raw_results.hits.total.value / page_size),
-        'results': [_format_hit(hit.to_dict()) for hit in raw_results]
-    }
-
-    if corrected_query != query:
-        response['correctedQuery'] = corrected_query
+    was_autocorrected = corrected_query != query
     
-    return response
+    num_pages = math.ceil(raw_results.hits.total.value / page_size)
+    
+    results = [SearchProduct.from_elastic(hit.to_dict()) for hit in raw_results]
+    
+    return SearchResponse(
+        results,
+        num_pages,
+        corrected_query,
+        was_autocorrected)
