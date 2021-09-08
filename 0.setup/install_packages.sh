d64fd1ecf403e4269a3a1354040171cd54da6eff
Ben Abrams <babrams@google.com>
2021-09-08 15:25:39 +0000

    Added one comment

--- 0.setup/install_packages.sh
+++ 0.setup/install_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Assumes you are running debian/ubuntu and have required sources already setup
-# Intended to be used on gLinux or in a GCP VM
+# Intended to be used on gLinux, cloud shell, or in a GCP VM
 
 sudo apt-get update
 sudo apt-get install -y google-cloud-sdk git npm
